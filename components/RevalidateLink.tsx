07299060d74cd11a0144b5254b6cdc026bcf4119
Jonathan Motes <jonathanmotes@gmail.com>
2022-03-24 11:36:41 -0500

    Adjust revalidation params.

--- components/RevalidateLink.tsx
+++ components/RevalidateLink.tsx
@@ -5,7 +5,7 @@ export type Props = {
 export default function RevalidateLink({ path }: Props) {
   return (
     <a
-      href={`/api/revalidate?path=${path}`}
+      href={`/api/revalidate?urlPath=${path}`}
       rel="noreferrer"
       style={{ display: "inline-block", marginTop: 20, color: "red" }}
       target="_blank"
--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,22 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  status: string
-}
+  status: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-
-  const { path } = req.query;
+  const { urlPath } = req.query;
 
   try {
-		await res.unstable_revalidate(path as string);
-	} catch (err) {
-		return res.status(500).json({ status: err as string });
-	}
+    await res.unstable_revalidate(urlPath as string);
+  } catch (err) {
+    return res.status(500).json({ status: err as string });
+  }
 
-  res.status(200).json({ status: `path '${path}' revalidated` })
+  res.status(200).json({ status: `path '${urlPath}' revalidated` });
 }
